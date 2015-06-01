7adc6095b43e90a94b8acc7f3dd3b1de4a301c02
Martti Natunen <martti.natunen@gmail.com>
2015-06-01 08:59:52 +0300

    Initial commit

---
+++ ImgStick/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImgStick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                ContextMenu cm = this.FindResource("cm") as ContextMenu;
+                cm.PlacementTarget = sender as Image;
+                cm.IsOpen = true;
+            }
+        }
+
+        private void menuSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "Common Image Files | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; *.gif; *.bmp;";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+
+                try
+                {
+                    Uri u = new Uri(dlg.FileName, UriKind.Absolute);
+                    img.Source = new BitmapImage(u);
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!");
+                }
+            }
+        }
+
+        private void menuClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
